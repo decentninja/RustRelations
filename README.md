efc0a1b47781d710eba98325392007add67e2ff7
sootn <encrypt.wave@gmail.com>
2014-12-23 13:24:10 +0100

    Relations

---
+++ README.md
@@ -0,0 +1,4 @@
+Rust Relations
+==============
+
+Hmm, implementing this relations stuff in rust would be a good exercise in both discrete mathematics and rust...
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,40 @@
+trait Relation<T> {
+	fn operation(&self, a: T, b: T) -> T;
+	fn is_transitive(&self) -> bool;
+	fn is_reflective(&self) -> bool;
+	fn is_symetric(&self) -> bool;
+	fn is_equivalence(&self) -> bool {
+		self.is_symetric() && self.is_reflective() && self.is_transitive()
+	}
+}
+
+
+struct Addition;
+
+impl Relation<int> for Addition {
+	fn operation(&self, a: int, b: int) -> int {
+		a + b
+	}
+	fn is_transitive(&self) -> bool {
+		true
+	}
+	fn is_reflective(&self) -> bool {
+		true
+	}
+	fn is_symetric(&self) -> bool {
+		true
+	}
+}
+
+
 #[test]
-fn it_works() {
+fn operation() {
+	let add = Addition;
+	assert!(add.operation(5, 6) == 11);
+}
+
+#[test]
+fn add_is_equivalence() {
+	let add = Addition;
+	assert!(add.is_equivalence());
 }
